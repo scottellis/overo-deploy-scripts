141fce01c975f18c3530de6876d13ff580c63054
Scott Ellis <scott@jumpnowtek.com>
2011-02-27 09:21:25 -0500

    Added better check for existing OETMP environment variable

--- copy_boot.sh
+++ copy_boot.sh
@@ -1,9 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "x${1}" = "x" ]; then
 	echo -e "\nUsage: ${0} <block device>\n"
 	exit 0
 fi
+
+if [[ -z "${OETMP}" ]]; then
+	echo "Working from local directory"
+else
+	echo "Using OETMP $OETMP"
+
+	if [ -d ${OETMP}/deploy/glibc/images/overo ]; then
+		cd ${OETMP}/deploy/glibc/images/overo
+	else
+		echo "Directory not found: ${OETMP}/deploy/glibc/images/overo"
+		exit 1
+	fi
+fi 
+
 
 DEV=/dev/${1}1
 
@@ -20,14 +34,24 @@ if [ -b $DEV ]; then
 	echo "Copying u-boot"
 	sudo cp u-boot-overo.bin /media/card/u-boot.bin
 
+	if [ -f boot.scr ]; then
+		echo "Copying boot.scr"
+		sudo cp boot.scr /media/card/boot.scr
+	fi
+
 	echo "Copying uImage"
 	sudo cp uImage-overo.bin /media/card/uImage
 
 	echo "Unmounting ${DEV}"
 	sudo umount ${DEV}
-
-	echo "Done"
 else
 	echo -e "\nBlock special file $DEV does not exist!\n"
 fi
 
+
+if [[ -z "${OETMP}" ]]; then
+	echo "Done"
+else
+	cd $OLDPWD
+	echo "Done"
+fi
