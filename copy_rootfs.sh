95e5581fb0b0f507840ee3d36eea23138f36ba27
Scott Ellis <scott@jumpnowtek.com>
2011-02-27 09:22:52 -0500

    Added better check for existing OETMP environment variable

--- copy_rootfs.sh
+++ copy_rootfs.sh
@@ -1,33 +1,49 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "x${1}" = "x" ]; then
 	echo -e "\nUsage: ${0} <block device>\n"
 	exit 0
 fi
 
+if [[ -z "${OETMP}" ]]; then
+	echo "Working from local directory"
+else
+	echo "Using OETMP $OETMP"
+
+	if [ -d ${OETMP}/deploy/glibc/images/overo ]; then
+		cd ${OETMP}/deploy/glibc/images/overo
+	else
+		echo "Directory not found: ${OETMP}/deploy/glibc/images/overo"
+		exit 1
+	fi
+fi 
+
 DEV=/dev/${1}2
 
 if [ -b $DEV ]; then
 	echo "Formatting $DEV as ext3"
-	sudo mkfs.ext3 $DEV
+	sudo mkfs.ext3 -L ROOT $DEV
 
 	echo "Mounting $DEV"
 	sudo mount $DEV /media/card
 
-	echo "Untar'ing rootfs to /dev/sdc2"
-	sudo tar -C /media/card -xvjf omap3-console-image-overo.tar.bz2
-	#sudo tar -C /media/card -xvjf network-test-image-overo.tar.bz2
+	echo "Untar'ing rootfs to /media/card"
+	sudo tar -C /media/card -xvjf work-image-overo.tar.bz2
 
-	echo "Sleeping for 60 seconds"
-	sleep 60
-
-	# echo "Fixing startup scripts"
-	# ./fix-startup-scripts.sh
+	if [ -f fix-minimal-startup-scripts.sh ]; then
+		echo "Fixing startup scripts"
+		./fix-minimal-startup-scripts.sh
+	fi
 
 	echo "Umounting $DEV"
 	sudo umount $DEV
-
-	echo "Done"
 else
 	echo "Block special file $DEV does not exist"
+fi
+
+if [[ -z "${OETMP}" ]]; then
+	echo "Done"
+else
+	cd $OLDPWD
+	echo "Done"
 fi
